bf6e67c5be5199bd145f04f44fd00b4253c093b9
xingrui05 <xingrui05@6813cf80-737e-ee97-ba8b-7d4d5a112c28>
2012-02-27 13:38:59 +0000

    add new project to find the speed limit of the forward sign.

---
+++ trunk/MSTSHelp/MSTSHelp/SignSpeedLimit/SignSpeedLimitDlg.h
@@ -0,0 +1,33 @@
+// SignSpeedLimitDlg.h : 头文件
+//
+
+#pragma once
+
+#include "Util.h"
+// CSignSpeedLimitDlg 对话框
+class CSignSpeedLimitDlg : public CDialog
+{
+// 构造
+public:
+	CSignSpeedLimitDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_SIGNSPEEDLIMIT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+	HANDLE m_hTrainProcess;
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
