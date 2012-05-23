cb3befc477f1aebd4dcafa09c94960837ed17aeb
xingrui05@gmail.com <xingrui05@gmail.com@6813cf80-737e-ee97-ba8b-7d4d5a112c28>
2012-05-23 15:22:40 +0000

    添加绘制曲线信息的代码

--- MSTSHelp/MSTSHelp/AirView/AirViewDlg.h
+++ MSTSHelp/MSTSHelp/AirView/AirViewDlg.h
@@ -4,6 +4,7 @@
 #pragma once
 
 
+#include "..\SignSpeedLimit\Util.h"
 // CAirViewDlg 对话框
 class CAirViewDlg : public CDialog
 {
@@ -20,14 +21,25 @@ protected:
 
 	// 实现
 protected:
+	HANDLE m_hTrainProcess;
 	HICON m_hIcon;
+	vector<SSectionInfo> m_vectSectionInfo;
+	vector<SSectionInfo> m_backVectSectionInfo;
 
 	// 生成的消息映射函数
 	virtual BOOL OnInitDialog();
 	void DrawTracks(CPaintDC *pDC);
+	void GetDataAndPaint();
+	void GetTrackData();
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnSize(UINT nType, int cx, int cy);
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+public:
+	afx_msg void OnDestroy();
+public:
+	afx_msg void OnBnClickedOk();
 };
