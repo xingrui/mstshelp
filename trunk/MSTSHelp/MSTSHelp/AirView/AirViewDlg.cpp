6312433c1d07a528c835ae30d68b993b0d8207e0
xingrui05@gmail.com <xingrui05@gmail.com@6813cf80-737e-ee97-ba8b-7d4d5a112c28>
2012-05-23 15:22:40 +0000

    添加绘制曲线信息的代码

--- trunk/MSTSHelp/MSTSHelp/AirView/AirViewDlg.cpp
+++ trunk/MSTSHelp/MSTSHelp/AirView/AirViewDlg.cpp
@@ -4,7 +4,7 @@
 #include "stdafx.h"
 #include "AirView.h"
 #include "AirViewDlg.h"
-
+#include "cmath"
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
@@ -31,6 +31,9 @@ BEGIN_MESSAGE_MAP(CAirViewDlg, CDialog)
 	ON_WM_QUERYDRAGICON()
 	//}}AFX_MSG_MAP
 	ON_WM_SIZE()
+	ON_WM_TIMER()
+	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDOK, &CAirViewDlg::OnBnClickedOk)
 END_MESSAGE_MAP()
 
 
@@ -43,6 +46,8 @@ BOOL CAirViewDlg::OnInitDialog()
 	//  执行此操作
 	SetIcon(m_hIcon, TRUE);			// 设置大图标
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
+	m_hTrainProcess = NULL;
+	SetTimer(0, 1000, NULL);
 	// TODO: 在此添加额外的初始化代码
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
@@ -87,16 +92,158 @@ void CAirViewDlg::DrawTracks(CPaintDC *pDC)
 	CRect rect;
 	GetClientRect(&rect);
 	pDC->SetMapMode(MM_ISOTROPIC);
-	pDC->SetWindowExt(1000, 1000);
+	pDC->SetWindowExt(2000, 2000);
 	pDC->SetViewportExt(rect.right, -rect.bottom);
-	pDC->SetViewportOrg(rect.right / 2, rect.bottom /2);
-	pDC->Rectangle(-500, -500, 500, 500);
+	pDC->SetViewportOrg(rect.right / 2, rect.bottom / 2);
+	float currentAngle = 0;
+
+	if (!m_vectSectionInfo.empty())
+		m_vectSectionInfo[0].fStart = 0;
+
+	float fCurrentX = 0, fCurrentY = 0;
+
+	for (size_t i = 0; i < m_vectSectionInfo.size(); ++i)
+	{
+		if (m_vectSectionInfo[i].nDirection == 0)
+		{
+			// 直轨道
+			float fLength = m_vectSectionInfo[i].fEnd - m_vectSectionInfo[i].fStart;
+			pDC->MoveTo(fCurrentX, fCurrentY);
+			fCurrentX += fLength * cos(currentAngle);
+			fCurrentY += fLength * sin(currentAngle);
+			pDC->LineTo(fCurrentX, fCurrentY);
+		}
+		else if (m_vectSectionInfo[i].nDirection == 1)
+		{
+			currentAngle += m_vectSectionInfo[i].fAngle;
+			// 右转
+		}
+		else
+		{
+			currentAngle -= m_vectSectionInfo[i].fAngle;
+			// 左转
+		}
+	}
+
+	//pDC->Rectangle(-500, -500, 500, 500);
 }
 
+void CAirViewDlg::GetDataAndPaint()
+{
+	if (!GetTrainHandle(m_hTrainProcess))
+	{
+		return;
+	}
+
+	if (!GetTrainPointer(m_hTrainProcess))
+	{
+		return;
+	}
+
+	GetTrackData();
+	OnPaint();
+}
+
+void CAirViewDlg::GetTrackData()
+{
+	m_vectSectionInfo.clear();
+	m_backVectSectionInfo.clear();
+	STrackInfo headInfo;
+	//headInfo is the information of the head of the train.
+	size_t trainInfo;
+	ReadTrainProcess(m_hTrainProcess, (void *)TRAIN_INFO_MEM, (LPVOID)&trainInfo, 4);
+	BOOL bIsForward;
+
+	if (trainInfo & 0x80) // Forward Or Backward
+	{
+		ReadTrainProcess(m_hTrainProcess, (void *)TAIL_TRACK_MEM, (LPVOID)&headInfo, sizeof(STrackInfo));
+		bIsForward = FALSE;
+	}
+	else
+	{
+		ReadTrainProcess(m_hTrainProcess, (void *)HEAD_TRACK_MEM, (LPVOID)&headInfo, sizeof(STrackInfo));
+		bIsForward = TRUE;
+	}
+
+	float forwardLength;
+	SVectorNode trackNode;
+	int nDirectOfHeadNode = headInfo.nDirection == bIsForward;
+	ReadTrainProcess(m_hTrainProcess, (void *)headInfo.vectorNodePtr, (LPVOID)&trackNode, sizeof(SVectorNode));
+
+	if (nDirectOfHeadNode)
+		forwardLength = - headInfo.fLocationInNode;
+	else
+		forwardLength = headInfo.fLocationInNode - trackNode.fTrackNodeLength;
+
+	int nDirectOfNextNode = nDirectOfHeadNode;
+	SVectorNode *nextNodePtr = headInfo.vectorNodePtr;
+	STrackSection *pSection;
+	ReadPointerMemory(m_hTrainProcess, (LPCVOID)0x80A118, &pSection, 4, 1, 0xC);
+
+	while (forwardLength < 4 * 1000 && nextNodePtr)
+	{
+		SVectorNode *currentNodePtr = nextNodePtr;
+		int nDirectOfCurrentNode = nDirectOfNextNode;
+		SVectorNode vectorNode;
+		ReadTrainProcess(m_hTrainProcess, (void *)currentNodePtr, (LPVOID)&vectorNode, sizeof(SVectorNode));
+		AddSectionInfo(forwardLength, vectorNode, m_vectSectionInfo, m_hTrainProcess, nDirectOfCurrentNode, !nDirectOfCurrentNode, pSection);
+		forwardLength += vectorNode.fTrackNodeLength;
+		/************************************************************************/
+		/* Get Next Node Pointer                                                */
+		/************************************************************************/
+		nextNodePtr = GetNextNode(m_hTrainProcess, vectorNode, currentNodePtr, nDirectOfCurrentNode, nDirectOfNextNode);
+	}
+
+	nDirectOfHeadNode = !nDirectOfHeadNode;
+	float backwardLength;
+
+	if (nDirectOfHeadNode)
+		backwardLength = - headInfo.fLocationInNode;
+	else
+		backwardLength = headInfo.fLocationInNode - trackNode.fTrackNodeLength;
+
+	int nDirectOfPrevNode = nDirectOfHeadNode;
+	SVectorNode *prevNodePtr = headInfo.vectorNodePtr;
+
+	while (backwardLength < 1000 && prevNodePtr)
+	{
+		SVectorNode *currentNodePtr = prevNodePtr;
+		int nDirectOfCurrentNode = nDirectOfPrevNode;
+		SVectorNode vectorNode;
+		ReadTrainProcess(m_hTrainProcess, (void *)currentNodePtr, (LPVOID)&vectorNode, sizeof(SVectorNode));
+		AddSectionInfo(backwardLength, vectorNode, m_backVectSectionInfo, m_hTrainProcess, nDirectOfCurrentNode, nDirectOfCurrentNode, pSection);
+		backwardLength += vectorNode.fTrackNodeLength;
+		/************************************************************************/
+		/* Get Next Node Pointer                                                */
+		/************************************************************************/
+		prevNodePtr = GetNextNode(m_hTrainProcess, vectorNode, currentNodePtr, nDirectOfCurrentNode, nDirectOfPrevNode);
+	}
+}
 
 void CAirViewDlg::OnSize(UINT nType, int cx, int cy)
 {
 	CDialog::OnSize(nType, cx, cy);
 	Invalidate();
 	UpdateWindow();
+}
+
+void CAirViewDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CDialog::OnTimer(nIDEvent);
+	//GetDataAndPaint();
+}
+
+void CAirViewDlg::OnDestroy()
+{
+	CDialog::OnDestroy();
+
+	if (m_hTrainProcess)
+		CloseHandle(m_hTrainProcess);
+}
+
+void CAirViewDlg::OnBnClickedOk()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	GetDataAndPaint();
 }
